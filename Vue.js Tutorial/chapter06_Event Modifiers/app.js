1daa91ee382b2f7996c356a72f85ce384ca79cc1
JISUNGIN <jsi920629@gmail.com>
2018-11-05 17:08:25 +0900

    Add: Vue JS 2 Tutorial chapter11까지 정리 The Net Ninja 유투브 강의 참고

---
+++ Vue.js Tutorial/chapter06_Event Modifiers/app.js
@@ -0,0 +1,31 @@
+// Vue 인스턴스를 생성합니다.
+
+new Vue({
+    // html 선언시 #을 붙입니다.
+    el: '#vue-app',
+
+    data: {
+        age: 27,
+        x: 0,
+        y: 0
+    },
+
+    methods: {
+        add: function(inc) {
+            this.age += inc;
+        },
+
+        subtract: function(dec) {
+            this.age -= dec;
+        },
+
+        updateXY: function(event) {
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        },
+        click: function() {
+            alert("Vue 이벤트로 이동하겠습니다.")
+        }
+
+    }
+});
---
+++ Vue.js Tutorial/chapter08_2-way Data Binding/app.js
@@ -0,0 +1,22 @@
+// Vue 인스턴스를 생성합니다.
+
+new Vue({
+    // html 선언시 #을 붙입니다.
+    el: '#vue-app',
+
+    data: {
+        name: '',
+        age: ''
+    },
+
+    methods: {
+        logName: function() {
+            console.log('you entered your name');
+        },
+
+        logAge: function() {
+            console.log('you entered your age');
+        }
+
+    }
+});
