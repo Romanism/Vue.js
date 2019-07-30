e9b637f6d6b7ed2979e4faee127619bd2a0845f5
JI <jsi920629@gmail.com>
2019-07-30 18:03:52 +0900

    Add: VueJS TODO APP 진행중

---
+++ Lecture/vue-todo/src/components/TodoList.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(todoItem, index) in todoItems" :key="index" class="shadow">
+        <i class="checkBtn fas fa-check" v-on:click="toggleComplete"></i>
+        <span class="textCompleted">{{todoItem.item}}</span>
+        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+          <i class="fas fa-trash-alt"></i>
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      todoItems: []
+    };
+  },
+
+  methods: {
+    removeTodo: function(todoItem, index) {
+      // 1. Browser에서 삭제
+      this.todoItems.splice(index, 1);
+
+      // 2. LocalStorage에서 삭제
+      localStorage.removeItem(todoItem);
+    },
+    toggleComplete: function() {}
+  },
+
+  created: function() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+          this.todoItems.push(
+            JSON.parse(localStorage.getItem(localStorage.key(i)))
+          );
+        }
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+</style>
